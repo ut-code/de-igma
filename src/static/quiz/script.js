648ac92032095900cb8cd1733d26fa9290f20dbe
Ryoma <tianzhongshenyilang64@gmail.com>
2023-08-18 15:01:39 +0900

    平文を箇条書きで表示

--- src/static/quiz/script.js
+++ src/static/quiz/script.js
@@ -1,5 +1,11 @@
 document.getElementById("fetch-button").onclick = async () => {
   const response = await fetch("/data");
-  const hirabun = await response.text();
-  alert(hirabun);
+  const hirabunArray = await response.json();
+  const ul = document.createElement("ul")
+  for (const dataRow of hirabunArray) {
+    const li = document.createElement("li");
+    li.innerHTML = dataRow.EnglishSentences;
+    ul.appendChild(li);
+  } 
+  document.body.appendChild(ul);
 };
